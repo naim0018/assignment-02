e56208e0940ba2b4bcd4737ea98ea76ad549da98
naim0018 <rakibhasan1101@gmail.com>
2024-06-09 20:23:11 +0600

    Add route not found

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import { ProductRoute } from './app/modules/Products/product.router'
 import { OrderRoute } from './app/modules/Orders/order.router'
@@ -17,6 +17,12 @@ app.get('/',(req:Request,res:Response)=>{
     res.send('Product Database Running')
 })
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).json({
+      success: false,
+      message: 'Route not found',
+    });
+  });
 
 
 export default app
